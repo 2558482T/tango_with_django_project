44e0286d122dc046999e9b80b108f072f6b7f250
Shao Tang <2558482t@student.gla.ac.uk>
2021-07-29 00:53:39 +0800

    Completed exercises for Chapter 3

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,5 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'), # map the empty string to the index view
+    path('about/', views.about, name="about"), # map the about view
 ]
