4895f99d9c428cc551e52406a02fe568071a919e
Shao Tang <2558482t@student.gla.ac.uk>
2021-07-29 20:15:15 +0800

    added slug for category, created category view and mapped its links

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,5 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'), # map the empty string to the index view
     path('about/', views.about, name="about"), # map the about view
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
