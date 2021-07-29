47400a1bbfc8c5fd4bb6d8b2c94b95dd73e2e4f1
Shao Tang <2558482t@student.gla.ac.uk>
2021-07-29 22:01:37 +0800

    Completed exercises for Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -11,10 +11,14 @@ def index(request):
     # Place the list in our context_dict dictionary (with our boldmessage!) 
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
+    # order the pages by the number of views in descending order.
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    # add pages list to the context_dict
+    context_dict['pages'] = page_list 
 
     # Render the response and send it to the client. 
     # Note that the first parameter is the template we wish to use.
